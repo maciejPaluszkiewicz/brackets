9ec99e886f54eeaa11e72cc01ec7e7a15627febe
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2020-03-10 19:11:35 +0100

    feat: implement bracket checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) return false;
+
+    const checkBracketsPair = (a, b) => {
+        for (let arr of bracketsConfig) {
+            if (arr[0] === a && arr[1] === b) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    let wasRemoved = true;
+    let arr = str.split("");
+
+    while (wasRemoved) {
+        wasRemoved = false;
+        for (let i = arr.length - 1; i > 0; --i) {
+            if (checkBracketsPair(arr[i - 1], arr[i])) {
+                arr.splice(i - 1, 2);
+                wasRemoved = true;
+            }
+        }
+    }
+    return arr.length === 0;
+};
